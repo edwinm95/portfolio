2be0c2a25c78b2de37de59a52a4b040be6d64ce6
Edwin Mwaniki <edwinmwaniki@Edwins-MBP.home>
2019-07-15 17:54:26 -0400

    Updates

---
+++ precache-manifest.3be910ddd35a514687b5f4864ae6768d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa2c56dd1a098da0929bada8b3d2ba7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ec74f2d2f497599c094f",
+    "url": "/portfolio/static/css/main.3aab9065.chunk.css"
+  },
+  {
+    "revision": "8f8cb96f4b2a9f1880a8",
+    "url": "/portfolio/static/js/2.44a9c448.chunk.js"
+  },
+  {
+    "revision": "ec74f2d2f497599c094f",
+    "url": "/portfolio/static/js/main.2d4cd1dc.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6c6db39a56fe8e8f19ba47c6a0770a18",
+    "url": "/portfolio/static/media/HelveticaNeue-Medium.6c6db39a.otf"
+  },
+  {
+    "revision": "9ce6b81d106aef68f9250c9247cf1d71",
+    "url": "/portfolio/static/media/HelveticaNeueLight.9ce6b81d.ttf"
+  },
+  {
+    "revision": "704d3b2818c5e16ac5520094625bf87a",
+    "url": "/portfolio/static/media/HelveticaNeueLightItalic.704d3b28.ttf"
+  },
+  {
+    "revision": "193d65be949ef1b74007ecb274e573c0",
+    "url": "/portfolio/static/media/HelveticaNowDisplayXBlk.193d65be.otf"
+  },
+  {
+    "revision": "766a39acecf44add34110b55b204c884",
+    "url": "/portfolio/static/media/homebuy.766a39ac.png"
+  },
+  {
+    "revision": "efa8b6b75ee1f194f7474527ca01def1",
+    "url": "/portfolio/static/media/landingpage.efa8b6b7.png"
+  },
+  {
+    "revision": "af7ce40b27bf4bb462390f7ad397e12b",
+    "url": "/portfolio/static/media/resume.af7ce40b.pdf"
+  }
+]);
